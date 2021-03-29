3ef67c840f6ac0f4d353608a8b95bc0ceef76b0a
novakovicn <nikolanovakovic2011@gmail.com>
2021-03-29 18:07:57 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(elems, {
+        format:"dd-mm-yyyy",
+        showClearBtn:true,
+        i18n:{
+            clear:"Poništi",
+            done:"Izaberi",
+            cancel:"Zatvori",
+            months:['Januar','Februar','Mart','April','Maj','Jun','Jul', 'Avgust','Septembar','Oktobar','Novembar','Decembar'],
+            onDraw:" "
+        }
+    });
+  });
+  
+  
+
+=======
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(elems, {
+        format:"dd-mm-yyyy",
+        showClearBtn:true,
+        i18n:{
+            clear:"Poništi",
+            done:"Izaberi",
+            cancel:"Zatvori",
+            months:['Januar','Februar','Mart','April','Maj','Jun','Jul', 'Avgust','Septembar','Oktobar','Novembar','Decembar'],
+            onDraw:" "
+        }
+    });
+  });
+  
+  
+
+>>>>>>> 711d810019b13a46563d9c24c43f0bbbbf052512
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".tikpka", {
+    strings: ["<h1>Djordje Antanaskovic</h1> <h2>Junior web developer</h2>"],
+    smartBackspace: true,
+    typeSpeed: 50,
+    startDelay:1000,
+    showCursor:false// Default value
+  });
